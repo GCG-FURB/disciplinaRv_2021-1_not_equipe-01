0fc70cadfbff593b2ebbe077c902529707c66855
richardalfarth <richard.curbani.alfarth@hotmail.com>
2021-06-16 18:10:01 -0300

    Atualizando

---
+++ unidade_4/Realidade Virtual - Farms/Assets/Scenes/Jogo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Jogo : MonoBehaviour
+{
+    private TextMeshPro textDinheiro;
+    public void inicio()
+    {
+        textDinheiro = GetComponent<TextMeshPro>();
+        double valorInicial = 1000;
+        textDinheiro = valorInicial.ToString();
+    }
+}
